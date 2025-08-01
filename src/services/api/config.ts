4c244c293365f2b61139e2bf8446aae9706eeef6
llebogo <lehlogonolo@broadsmart.co.za>
2025-08-01 09:12:48 +0200

    changed supabase database in config

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Supabase REST API configuration
-const SUPABASE_URL = 'https://yfkdcqgmyyrcxppxcswz.supabase.co';
+const SUPABASE_URL = 'https://innpnyojhyedfrtnaxsi.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlma2RjcWdteXlyY3hwcHhjc3d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2NTc0OTcsImV4cCI6MjA2ODIzMzQ5N30.dlEDNLZa6kLz_BCiGC1JJX83ceYTRRNc8mn1R70sOIw';
 
 // Create axios instance for Supabase REST API
